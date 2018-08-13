9b217ca1f8bcf9bc3723312ef0427022cebaa3b8
Geetha <kgeethareddy81@gmail.com>
2018-08-12 23:08:52 -0400

    added files to repo

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './components/common/Header';
+
+
+const App =() =>{
+    return(
+    <div>
+    <Header />
+    <h1> React Coin</h1>;
+    </div>)
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
